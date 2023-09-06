19179cb5af11243d7b61e655ba7cca5e54c675ae
carole tierney <caroleatierney@gmail.com>
2023-09-06 19:14:22 -0400

    test first commit

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Log In</h3>
+
+            </div>
+        )
+    }
+}
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+        )
+    }
+}
